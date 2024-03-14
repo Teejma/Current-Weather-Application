5dfe8a7fb3799dd1d18b11b5920fbb8a0282ad9c
Teejma <160725081+Teejma@users.noreply.github.com>
2024-03-14 12:36:32 +0200

    added current temperature

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+function displayWeather (response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML = `${temperature}`;
+let h1 = document.querySelector("h1");
+h1.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+let apiKey = "16b4b9bft43b0d3a60caoa604550fe80";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiURL).then(displayWeather); 
+}
 function displayCity (event) {
     event.preventDefault();
     let enterCityInput = document.querySelector("#enter-city");
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = `${enterCityInput.value}`; 
+     searchCity (enterCityInput.value);
+  
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", displayCity);
+
+
+searchCity("lisbon");
