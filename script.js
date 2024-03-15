26a6e5eb3bed1493309c412cdfb0de8b033c593e
Teejma <160725081+Teejma@users.noreply.github.com>
2024-03-15 08:17:03 +0200

    added more weather elements

--- script.js
+++ script.js
@@ -4,7 +4,40 @@ function displayWeather (response) {
     temperatureElement.innerHTML = `${temperature}`;
 let h1 = document.querySelector("h1");
 h1.innerHTML = response.data.city;
+let descriptionElement= document.querySelector("#description");
+descriptionElement.innerHTML = response.data.condition.description;
+let humidityPercentageElement= document.querySelector("#humidity-percentage");
+humidityPercentageElement.innerHTML = `${response.data.temperature.humidity}%`;
+let windSpeedElement= document.querySelector("#wind-speed");
+windSpeedElement.innerHTML= `${response.data.wind.speed}km/h`;
+let dateTimeElement = document.querySelector("#date-time");
+let date = new Date(response.data.time * 1000);
+dateTimeElement.innerHTML= displayDate(date);
 }
+function displayDate (date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+     
+
 
 function searchCity(city) {
 let apiKey = "16b4b9bft43b0d3a60caoa604550fe80";
@@ -23,3 +56,4 @@ form.addEventListener("submit", displayCity);
 
 
 searchCity("lisbon");
+
